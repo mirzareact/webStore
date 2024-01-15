df1ffa20f82c2f9a18ecb3b6ef8fff4647786a82
mirzareact <hodzic04mirza@gmail.com>
2024-01-15 23:41:12 +0100

    mongo edit

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import productRoute from "./routes/productRoute.js";
-import stripeRoute from "./routes/stripeRoute.js"
+import stripeRoute from "./routes/stripeRoute.js";
 
 const app = express();
 
@@ -12,8 +12,6 @@ const app = express();
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(cors());
 app.use(express.json());
-
-
 
 // Define routes
 app.get("/", (req, res) => {
@@ -24,10 +22,9 @@ app.get("/", (req, res) => {
 app.use("/proizvodi", productRoute);
 app.use("/checkout-stripe", stripeRoute);
 
-
 // Connect to MongoDB and start the server
 mongoose
-  .connect(mongoDBURL,{ useUnifiedTopology: true })
+  .connect(mongoDBURL, { useUnifiedTopology: true })
   .then(() => {
     console.log("App connected to the database");
     app.listen(process.env.PORT || PORT, () => {
