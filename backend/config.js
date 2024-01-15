8c9e0cb52075d329318749eb7074c659d48857f0
mirzareact <hodzic04mirza@gmail.com>
2024-01-15 23:44:28 +0100

    mongo edit v2

--- backend/config.js
+++ backend/config.js
@@ -2,9 +2,8 @@ export const PORT = 5555;
 export const BACKENDLOCALHOSTURL = "http://localhost:5555";
 export const FRONTENDLOCALHOSTURL = "http://localhost:5173";
 export const mongoDBURL =
-  "mongodb+srv://webshop:hodzic04mirza@webshop.4yaogah.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://webshop:hodzic04mirza@ac-evnfea5-shard-00-02.4yaogah.mongodb.net,ac-evnfea5-shard-00-00.4yaogah.mongodb.net,ac-evnfea5-shard-00-01.4yaogah.mongodb.net/test?authSource=admin&replicaSet=atlas-mekthv-shard-0&retryWrites=true&w=majority&tls=true";
 export const StripeAPIKEYPRIVATE =
   "sk_test_51OXrlzJpULZZj4e9aYQ9O91cMLvsPslIXp7LDzikCheyqXIj7hTI159bnyzNHHTs6YFQCzvaeSJVWzQlwKYSl15K00sbqkhvdo";
-  export const StripeAPIKEYPUBLIC =
-    "pk_test_51OXrlzJpULZZj4e9NG8ZRHdHzAdDdhKiYo17UHPq6jMUA4h3cFVkhDHhSHY7FSjotD5VzEYK9uZg7kACLQj1zhAu00lgiFlioK";
-
+export const StripeAPIKEYPUBLIC =
+  "pk_test_51OXrlzJpULZZj4e9NG8ZRHdHzAdDdhKiYo17UHPq6jMUA4h3cFVkhDHhSHY7FSjotD5VzEYK9uZg7kACLQj1zhAu00lgiFlioK";
