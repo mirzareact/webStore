295507a75ce151ee535927a2c558b468de352e15
mirzareact <hodzic04mirza@gmail.com>
2024-01-17 20:08:04 +0100

    mongo edit v6 alo

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ export const PORT = 10000;
 export const BACKENDLOCALHOSTURL = "http://localhost:5555";
 export const FRONTENDLOCALHOSTURL = "http://localhost:5173";
 export const mongoDBURL =
-  "mongodb+srv://webshop:hodzic04mirza@ac-evnfea5-shard-00-02.4yaogah.mongodb.net,ac-evnfea5-shard-00-00.4yaogah.mongodb.net,ac-evnfea5-shard-00-01.4yaogah.mongodb.net/test?authSource=admin&replicaSet=atlas-mekthv-shard-0&retryWrites=true&w=majority&tls=true";
+  "mongodb+srv://webshop:hodzic04mirza@gmail.com@webshop.4yaogah.mongodb.net/?retryWrites=true&w=majority";
 export const StripeAPIKEYPRIVATE =
   "sk_test_51OXrlzJpULZZj4e9aYQ9O91cMLvsPslIXp7LDzikCheyqXIj7hTI159bnyzNHHTs6YFQCzvaeSJVWzQlwKYSl15K00sbqkhvdo";
 export const StripeAPIKEYPUBLIC =
